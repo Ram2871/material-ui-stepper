3b9fbade38019c3419dc00f8d803aaac61d7a3fa
Ram2871 <ramchandra@r2logics.com>
2023-04-22 18:30:20 +0530

    Ram Commit

--- src/LinearStepper.js
+++ src/LinearStepper.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
 import {
   Typography,
   TextField,
@@ -8,8 +9,12 @@ import {
   StepLabel,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
-import { useForm, formProvider, useFormContext, Controller, FormProvider } from 'react-hook-form';
+import {
+  useForm,
+  Controller,
+  FormProvider,
+  useFormContext,
+} from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -25,158 +30,258 @@ function getSteps() {
     "Payment",
   ];
 }
-
-const BasicInfo = () => {
+const BasicForm = () => {
+  const { control } = useFormContext();
   return (
     <>
-      <TextField
-        id="first-name"
-        label="First Name"
-        variant="outlined"
-        placeholder="Enter Your First Name"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="firstName"
+        render={({ field }) => (
+          <TextField
+            id="first-name"
+            label="First Name"
+            variant="outlined"
+            placeholder="Enter Your First Name"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="last-name"
-        label="Last Name"
-        variant="outlined"
-        placeholder="Enter Your Last Name"
-        fullWidth
-        margin="normal"
-        name="lastName"
-      />
-      <TextField
-        id="nick-name"
-        label="Nick Name"
-        variant="outlined"
-        placeholder="Enter Your Nick Name"
-        fullWidth
-        margin="normal"
-        name="nickName"
-      />
-    </>
-  )
-};
 
-const ContactInfo = () => {
+      <Controller
+        control={control}
+        name="lastName"
+        render={({ field }) => (
+          <TextField
+            id="last-name"
+            label="Last Name"
+            variant="outlined"
+            placeholder="Enter Your Last Name"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="nickName"
+        render={({ field }) => (
+          <TextField
+            id="nick-name"
+            label="Nick Name"
+            variant="outlined"
+            placeholder="Enter Your Nick Name"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
+      />
+    </>
+  );
+};
+const ContactForm = () => {
+  const { control } = useFormContext();
   return (
     <>
-      <TextField
-        id="email"
-        label="E-mail"
-        variant="outlined"
-        placeholder="Enter Your E-mail Address"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="emailAddress"
+        render={({ field }) => (
+          <TextField
+            id="email"
+            label="E-mail"
+            variant="outlined"
+            placeholder="Enter Your E-mail Address"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="phone-number"
-        label="Phone Number"
-        variant="outlined"
-        placeholder="Enter Your Phone Number"
-        fullWidth
-        margin="normal"
+
+      <Controller
+        control={control}
         name="phoneNumber"
+        render={({ field }) => (
+          <TextField
+            id="phone-number"
+            label="Phone Number"
+            variant="outlined"
+            placeholder="Enter Your Phone Number"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="alternate-phone"
-        label="Alternate Phone"
-        variant="outlined"
-        placeholder="Enter Your Alternate Phone"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="alternatePhone"
+        render={({ field }) => (
+          <TextField
+            id="alternate-phone"
+            label="Alternate Phone"
+            variant="outlined"
+            placeholder="Enter Your Alternate Phone"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
     </>
-  )
+  );
 };
-const PersonalInfo = () => {
+const PersonalForm = () => {
+  const { control } = useFormContext();
   return (
     <>
-      <TextField
-        id="address1"
-        label="Address 1"
-        variant="outlined"
-        placeholder="Enter Your Address 1"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="address1"
+        render={({ field }) => (
+          <TextField
+            id="address1"
+            label="Address 1"
+            variant="outlined"
+            placeholder="Enter Your Address 1"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="address2"
-        label="Address 2"
-        variant="outlined"
-        placeholder="Enter Your Address 2"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="address2"
+        render={({ field }) => (
+          <TextField
+            id="address2"
+            label="Address 2"
+            variant="outlined"
+            placeholder="Enter Your Address 2"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="country"
-        label="Country"
-        variant="outlined"
-        placeholder="Enter Your Country Name"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="country"
+        render={({ field }) => (
+          <TextField
+            id="country"
+            label="Country"
+            variant="outlined"
+            placeholder="Enter Your Country Name"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
     </>
-  )
+  );
 };
-const PaymentInfo = () => {
+const PaymentForm = () => {
+  const { control } = useFormContext();
   return (
     <>
-      <TextField
-        id="cardNumber"
-        label="Card Number"
-        variant="outlined"
-        placeholder="Enter Your Card Number"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="cardNumber"
+        render={({ field }) => (
+          <TextField
+            id="cardNumber"
+            label="Card Number"
+            variant="outlined"
+            placeholder="Enter Your Card Number"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="cardMonth"
-        label="Card Month"
-        variant="outlined"
-        placeholder="Enter Your Card Month"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="cardMonth"
+        render={({ field }) => (
+          <TextField
+            id="cardMonth"
+            label="Card Month"
+            variant="outlined"
+            placeholder="Enter Your Card Month"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
-      <TextField
-        id="cardYear"
-        label="Card Year"
-        variant="outlined"
-        placeholder="Enter Your Card Year"
-        fullWidth
-        margin="normal"
+      <Controller
+        control={control}
         name="cardYear"
+        render={({ field }) => (
+          <TextField
+            id="cardYear"
+            label="Card Year"
+            variant="outlined"
+            placeholder="Enter Your Card Year"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
       />
     </>
-  )
+  );
 };
 
 function getStepContent(step) {
   switch (step) {
-    case 0: return <BasicInfo />;
-    case 1: return <ContactInfo />;
-    case 2: return <PersonalInfo />;
-    case 3: return <PaymentInfo />;
-    default: return "unknown step";
+    case 0:
+      return <BasicForm />;
+
+    case 1:
+      return <ContactForm />;
+    case 2:
+      return <PersonalForm />;
+    case 3:
+      return <PaymentForm />;
+    default:
+      return "unknown step";
   }
 }
 
 const LinaerStepper = () => {
   const classes = useStyles();
+
+  const methods = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      nickName: "",
+      emailAddress: "",
+      phoneNumber: "",
+      alternatePhone: "",
+      address1: "",
+      address2: "",
+      country: "",
+      cardNumber: "",
+      cardMonth: "",
+      cardYear: "",
+    },
+  });
   const [activeStep, setActiveStep] = useState(0);
+  const [posts, setPosts] = useState([]);
+
   const [skippedSteps, setSkippedSteps] = useState([]);
   const steps = getSteps();
-  const methods = useForm();
-
 
   const isStepOptional = (step) => {
     return step === 1 || step === 2;
@@ -186,9 +291,23 @@ const LinaerStepper = () => {
     return skippedSteps.includes(step);
   };
 
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-    setSkippedSteps(skippedSteps.filter((skipItem) => skipItem !== activeStep));
+  const handleNext = (data) => {
+    console.log('object');
+    console.log(data);
+    if (activeStep == steps.length - 1) {
+      fetch("https://jsonplaceholder.typicode.com/comments")
+        .then((data) => data.json())
+        .then((res) => {
+          console.log("res", res);
+          setPosts(res);
+          setActiveStep(activeStep + 1);
+        });
+    } else {
+      setActiveStep(activeStep + 1);
+      setSkippedSteps(
+        skippedSteps.filter((skipItem) => skipItem !== activeStep)
+      );
+    }
   };
 
   const handleBack = () => {
@@ -202,9 +321,18 @@ const LinaerStepper = () => {
     setActiveStep(activeStep + 1);
   };
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => {
+  //       console.log(response);
+  //       setPosts(response);
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // }, []);
+  console.log('posts', posts);
 
   return (
     <div>
@@ -233,16 +361,26 @@ const LinaerStepper = () => {
           );
         })}
       </Stepper>
+      <div className="container bg-danger">
+        {posts?.map((post) => (
+          <div className="d-flex" key={post.id}>
+           <small>{post.name}</small> 
+           <small>{post.email}</small> 
+          </div>
+        ))
+        }
+      </div>
 
       {activeStep === steps.length ? (
         <Typography variant="h3" align="center">
-          Thank You
+          Thank You 😎
         </Typography>
       ) : (
         <>
           <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(handleNext)}>
+              {getStepContent(activeStep)}
 
-            <form onSubmit={methods.handleSubmit(onSubmit)}>{getStepContent(activeStep)}
               <Button
                 className={classes.button}
                 disabled={activeStep === 0}
@@ -264,13 +402,15 @@ const LinaerStepper = () => {
                 className={classes.button}
                 variant="contained"
                 color="primary"
-                onClick={handleNext}
+                // onClick={handleNext}
                 type="submit"
               >
                 {activeStep === steps.length - 1 ? "Finish" : "Next"}
               </Button>
             </form>
           </FormProvider>
+
+
         </>
       )}
     </div>
